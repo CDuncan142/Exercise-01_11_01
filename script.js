fe3e5ebd7d2335dd34210ce17e57df28da15add6
CDuncan142 <CDuncan142@West-MEC.org>
2018-08-31 18:27:34 -0700

    table cells, attribution
    inserted data from responce into table
    added atribution under table
    Set attribution text to appear on the page

--- script.js
+++ script.js
@@ -17,7 +17,6 @@ var httpRequest = false; // Have an XHR object?
 
 //function to get a request object
 function getRequestObject() {
-	alert("getRequestObject");
 	try {
 		httpRequest = new XMLHttpRequest();
 	} catch (errorMessage) {
@@ -39,9 +38,48 @@ function fillWeather() {
 		var dayOfWeek = dateValue.getDay(); //returns index
 		var rows = document.querySelectorAll("section.week table tbody tr");
 		document.querySelector("section.week table caption").innerHTML = selectedCity;
+		//retrieve the row cells
+		for (var i = 0; i < rows.length; i++) {
+			var firstCell = rows[i].getElementsByTagName("td")[0];
+			var secondCell = rows[i].getElementsByTagName("td")[1];
+			var thirdCell = rows[i].getElementsByTagName("td")[2];
+			// set the weekday for the row
+			firstCell.innerHTML = days[dayOfWeek];
+			if (dayOfWeek + 1 === 7) {
+				dayOfWeek = 0;
+			} else {
+				dayOfWeek++;
+			}
+			// get the percentage of sunlight
+			var sun = Math.round((1 - weatherReport.daily.data[i].cloudCover) * 100, 0);
+			//change colors of sun symbol based on percentage
+			if (sun > 90) {
+				secondCell.style.color = "rgb(255, 171, 0)";
+			} else if (sun > 80) {
+				secondCell.style.color = "rgb(255, 179, 25)";
+			} else if (sun > 70) {
+				secondCell.style.color = "rgb(255, 188, 51)";
+			} else if (sun > 60) {
+				secondCell.style.color = "rgb(255, 196, 77)";
+			} else if (sun > 50) {
+				secondCell.style.color = "rgb(255, 205, 102)";
+			} else if (sun > 40) {
+				secondCell.style.color = "rgb(255, 213, 128)";
+			} else if (sun > 30) {
+				secondCell.style.color = "rgb(255, 221, 153)";
+			} else if (sun > 20) {
+				secondCell.style.color = "rgb(255, 230, 179)";
+			} else if (sun > 10) {
+				secondCell.style.color = "rgb(255, 238, 204)";
+			} else {
+				secondCell.style.color = "rgb(255, 247, 230)";
+			}
+			secondCell.style.fontSize = "2.5em";
+			thirdCell.innerHTML = sun + "%";
+		}
 		document.querySelector("section.week table caption").style.display = "block";
 		document.querySelector("section.week table").style.display = "inline-block";
-
+		document.querySelector("section.week p.credit").style.display = "block";
 
 	}
 
@@ -94,7 +132,6 @@ function getWeather(evt) {
 	//test for XHR object
 	if (!httpRequest) {
 		httpRequest = getRequestObject();
-		alert("httpRequest");
 	}
 	//protect against an open request
 	httpRequest.abort();
